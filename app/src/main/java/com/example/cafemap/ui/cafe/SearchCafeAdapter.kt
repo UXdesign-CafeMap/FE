44c1032d92a5e167019608252d13d9fd609912aa
hyunn522 <tjgus52213@gmail.com>
2023-11-26 12:52:18 +0900

    feat: SearchCafe recyclerview binding 1차 완료

--- app/src/main/java/com/example/cafemap/ui/cafe/SearchCafeAdapter.kt
+++ app/src/main/java/com/example/cafemap/ui/cafe/SearchCafeAdapter.kt
@@ -1,21 +1,44 @@
 package com.example.cafemap.ui.cafe
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.cafemap.databinding.ItemSearchCafeListBinding
 
 class SearchCafeAdapter(var items : List<String>) : RecyclerView.Adapter<SearchCafeAdapter.ViewHolder>() {
+
+    inner class ViewHolder(val binding: ItemSearchCafeListBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item : String) {
+//            binding.root.setOnClickListener {
+//                itemClickListener!!.onItemClicked(item.challengeId)
+//            }
+
+//            binding.tvMcTitle.text = item.contents.title
+//            binding.tvItemMyChallengePercent.text = (item.progressRate * 100).toInt().toString() + "%"
+//            binding.cvMcProgress.layoutParams.width = (360 * item.progressRate).toInt()
+
+        }
+
+    }
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): SearchCafeAdapter.ViewHolder {
-        TODO("Not yet implemented")
+    ): ViewHolder {
+        val binding = ItemSearchCafeListBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: SearchCafeAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(items[position])
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return items.size
+    }
+
+    fun setData(list:List<String>) {
+        items = list
+        notifyDataSetChanged()
     }
 }
