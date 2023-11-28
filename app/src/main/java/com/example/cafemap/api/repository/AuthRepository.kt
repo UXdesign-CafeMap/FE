d81d9e2bc4ad846ef9fcbad5348be5100ceb8061
dpwlsz <dpwls0875@konkuk.ac.kr>
2023-11-29 03:09:25 +0900

    feat(signup): api call and layout

--- app/src/main/java/com/example/cafemap/api/repository/AuthRepository.kt
+++ app/src/main/java/com/example/cafemap/api/repository/AuthRepository.kt
@@ -1,16 +1,15 @@
 package com.example.cafemap.api.repository
 
+import com.example.cafemap.api.model.SignInRequest
+import com.example.cafemap.api.model.SignUpRequest
 import retrofit2.Call
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
+import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthRepository {
-    @FormUrlEncoded
     @POST("member/signup")
-    fun signUp(@Field("email") email: String, @Field("password") password: String): Call<Void>
+    fun signUp(@Body request: SignUpRequest): Call<Void>
 
-    @FormUrlEncoded
     @POST("member/signin")
-    fun signIn(@Field("email") email: String, @Field("password") password: String): Call<Void>
+    fun signIn(@Body request: SignInRequest): Call<Void>
 }
--- app/src/main/java/com/example/cafemap/api/service/AuthService.kt
+++ app/src/main/java/com/example/cafemap/api/service/AuthService.kt
@@ -1,6 +1,8 @@
 package com.example.cafemap.api.service
 
 import com.example.cafemap.api.RetrofitClient
+import com.example.cafemap.api.model.SignInRequest
+import com.example.cafemap.api.model.SignUpRequest
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -12,28 +14,32 @@ object AuthService {
     }
 
     fun signUp(email: String, password: String, callback: (Boolean) -> Unit){
-        userRepository.signUp(email, password).enqueue(object : Callback<Void> {
+        val signUpRequest = SignUpRequest(email, password)
+        userRepository.signUp(signUpRequest).enqueue(object : Callback<Void> {
             override fun onResponse(call: Call<Void>, response: Response<Void>) {
                 // 성공 처리...
-                callback(true);
+                callback(true)
                 // set preference
-
             }
 
             override fun onFailure(call: Call<Void>, t: Throwable) {
                 // 실패 처리...
+                callback(false)
             }
         })
     }
 
     fun signIn (email: String, password: String, callback: (Boolean) -> Unit){
-        userRepository.signIn(email, password).enqueue(object : Callback<Void> {
+        val signInRequest = SignInRequest(email, password)
+        userRepository.signIn(signInRequest).enqueue(object : Callback<Void> {
             override fun onResponse(call: Call<Void>, response: Response<Void>) {
                 // 성공 처리...
-                callback(true);
+                callback(true)
             }
+
             override fun onFailure(call: Call<Void>, t: Throwable) {
                 // 실패 처리...
+                callback(false)
             }
         })
     }
