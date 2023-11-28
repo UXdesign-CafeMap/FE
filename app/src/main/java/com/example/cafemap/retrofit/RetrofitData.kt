512063dee8a9ca9c606d81f7f64b8aff828a6662
hyunn522 <tjgus52213@gmail.com>
2023-11-28 21:34:14 +0900

    feat: retrofit initial setting

---
+++ app/src/main/java/com/example/cafemap/retrofit/RetrofitData.kt
@@ -0,0 +1,8 @@
+package com.example.cafemap.retrofit
+
+import com.google.gson.annotations.SerializedName
+
+//data class Border(
+//    @SerializedName("id") val id: Int,
+//    @SerializedName("memberBorders") val memberBorders: List<MemberBorder>
+//)
