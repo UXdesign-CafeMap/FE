5345b9802edbee688121aeb2e984ad4841b13b5a
dpwlsz <dpwls0875@konkuk.ac.kr>
2023-11-28 22:53:03 +0900

    merge conflict

--- app/src/main/java/com/example/cafemap/ui/login/SignUpActivity.kt
+++ app/src/main/java/com/example/cafemap/ui/login/SignUpActivity.kt
@@ -1,47 +1,6 @@
 package com.example.cafemap.ui.login
 
 import android.os.Bundle
-<<<<<<< HEAD
-import androidx.appcompat.app.AppCompatActivity
-import com.example.cafemap.databinding.ActivitySignUpBinding
-
-class SignUpActivity : AppCompatActivity() {
-    lateinit var _binding: ActivitySignUpBinding
-
-    val binding : ActivitySignUpBinding get() = _binding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        _binding = ActivitySignUpBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        initlayout()
-
-//        RetrofitUtil.getRetrofitUtil().getProofPosts(challengeId).enqueue(object : Callback<GetProofPostsResponse> {
-//            override fun onResponse(
-//                call: Call<GetProofPostsResponse>,
-//                response: Response<GetProofPostsResponse>
-//            ) {
-//                if(response.isSuccessful) {
-//                    recordViewModel.addRecords(response.body()!!.proofPosts)
-//                } else {
-//                    Log.d("seohyun", response.errorBody().toString())
-//                }
-//            }
-//
-//            override fun onFailure(call: Call<GetProofPostsResponse>, t: Throwable) {
-//                Log.d("seohyun", t.message.toString())
-//            }
-    }
-
-    fun initlayout() {
-
-        // 회원가입 버튼 클릭 시 홈화면으로 이동 + alert("회원가입이 완료됐습니다")
-//        binding.btLiLoginButton.setOnClickListener {
-//            val i = Intent(applicationContext, MainActivity::class.java)
-//            startActivity(i)
-//        }
-=======
 import android.util.Patterns.EMAIL_ADDRESS
 import android.widget.Button
 import android.widget.EditText
@@ -50,8 +9,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.cafemap.R
 import com.example.cafemap.api.RetrofitClient
 import com.example.cafemap.api.service.UserService
-
-
 class SignUpActivity : AppCompatActivity() {
 
     private lateinit var emailEditText: EditText
@@ -117,6 +74,5 @@ class SignUpActivity : AppCompatActivity() {
                 Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
             }
         }
->>>>>>> 131cadc (feat(login): add retrofit)
     }
 }
