849e11d0b55cc30106f7f38e7e447429b48a10bb
dpwlsz <dpwls0875@konkuk.ac.kr>
2023-11-30 02:10:40 +0900

    feat: add google storage

--- app/src/main/java/com/example/cafemap/ui/cafe/PostReviewActivity.kt
+++ app/src/main/java/com/example/cafemap/ui/cafe/PostReviewActivity.kt
@@ -1,12 +1,18 @@
 package com.example.cafemap.ui.cafe
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
 import com.example.cafemap.databinding.ActivityPostReviewBinding
-import com.example.cafemap.databinding.ActivitySignUpBinding
+import com.google.firebase.storage.FirebaseStorage
+import java.io.IOException
+import java.util.UUID
+
 
 class PostReviewActivity : AppCompatActivity() {
-
+    private val PICK_IMAGE_REQUEST = 1
     lateinit var _binding: ActivityPostReviewBinding
 
     val binding : ActivityPostReviewBinding get() = _binding
@@ -18,29 +24,41 @@ class PostReviewActivity : AppCompatActivity() {
 
         initlayout()
 
-//        RetrofitUtil.getRetrofitUtil().getProofPosts(challengeId).enqueue(object : Callback<GetProofPostsResponse> {
-//            override fun onResponse(
-//                call: Call<GetProofPostsResponse>,
-//                response: Response<GetProofPostsResponse>
-//            ) {
-//                if(response.isSuccessful) {
-//                    recordViewModel.addRecords(response.body()!!.proofPosts)
-//                } else {
-//                    Log.d("seohyun", response.errorBody().toString())
-//                }
-//            }
-//
-//            override fun onFailure(call: Call<GetProofPostsResponse>, t: Throwable) {
-//                Log.d("seohyun", t.message.toString())
-//            }
     }
 
     fun initlayout() {
-
-        // 회원가입 버튼 클릭 시 홈화면으로 이동 + alert("회원가입이 완료됐습니다")
-//        binding.btLiLoginButton.setOnClickListener {
-//            val i = Intent(applicationContext, MainActivity::class.java)
-//            startActivity(i)
-//        }
+        binding.addPhoto.setOnClickListener { openGallery() }
     }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.data != null) {
+            val selectedImage = data.data
+            try {
+                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedImage)
+                binding.addPhoto.setImageBitmap(bitmap) // ImageView에 이미지 표시
+                uploadImageToFirebase(selectedImage) // Firebase로 이미지 업로드
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun uploadImageToFirebase(imageUri: Uri?) {
+        val fileRef =
+            FirebaseStorage.getInstance().getReference("uploads/" + UUID.randomUUID().toString())
+        fileRef.putFile(imageUri!!)
+            .addOnSuccessListener {
+                // 업로드 성공 시 동작
+            }
+            .addOnFailureListener {
+                // 업로드 실패 시 동작
+            }
+    }
+
 }
