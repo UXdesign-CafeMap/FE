e85437d104c2a827a5ee9cc098c3070617cce219
dpwlsz <dpwls0875@konkuk.ac.kr>
2023-12-03 22:41:57 +0900

    feat(mypage:nickname-email)

--- app/src/main/java/com/example/cafemap/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/com/example/cafemap/ui/mypage/MyPageFragment.kt
@@ -1,5 +1,6 @@
 package com.example.cafemap.ui.mypage
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -20,6 +21,19 @@ class MyPageFragment : Fragment() {
     ): View? {
         _binding = FragmentMyPageBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        val sp = requireActivity().getSharedPreferences("pref", Context.MODE_PRIVATE)
+        val nickname = sp.getString("nickname", "No Nickname") // 찾지 못했을 경우 기본값
+        val email = sp.getString("email", "No Email") // 찾지 못했을 경우 기본값
+
+        binding.tvMpUserName.text = nickname
+        binding.tvMpUserEmail.text = email
+
         return root
     }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
