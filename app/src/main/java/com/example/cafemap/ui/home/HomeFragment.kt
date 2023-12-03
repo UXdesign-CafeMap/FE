ff0aa15355a3d6dbdfb471729b79128de64efa58
hyunn522 <tjgus52213@gmail.com>
2023-12-03 13:33:32 +0900

    feat: 마커 클릭 시 보일 레이아웃 추가

--- app/src/main/java/com/example/cafemap/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/cafemap/ui/home/HomeFragment.kt
@@ -9,6 +9,7 @@ import android.graphics.drawable.BitmapDrawable
 import android.location.Location
 import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -92,74 +93,42 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
         marker1.position(cafe1)
         marker1.title("마우스래빗")
 
-        val drawable = ResourcesCompat.getDrawable(resources, R.drawable.marker_400, null)
-        val bitmap = drawable?.toBitmap()
-        val marker1Img = bitmap?.let { Bitmap.createScaledBitmap(it, 78, 96, false) }
+        val drawable1 = ResourcesCompat.getDrawable(resources, R.drawable.marker_400, null)
+        val bitmap1 = drawable1?.toBitmap()
+        val marker1Img = bitmap1?.let { Bitmap.createScaledBitmap(it, 78, 96, false) }
         marker1.icon(marker1Img?.let { BitmapDescriptorFactory.fromBitmap(it) })
 
         // map에 marker들 추가
-        mMap.addMarker(marker1)
+        val addedMarker1 = mMap.addMarker(marker1)
+        addedMarker1?.tag = 1
 
         // 각 marker에 click event 설정
         googleMap.setOnMarkerClickListener { clickedMarker ->
-            // 클릭된 마커 처리
-            if(clickedMarker == marker1){
+            // tag에 따라 클릭된 마커 처리
+            when (clickedMarker.tag) {
+                1 -> {
+                    Log.d("seohyunMarker",clickedMarker.tag.toString())
+                    updateNearCafe(1)
+                }
+                2 -> {
 
+                }
             }
-            false
+            true
         }
+        // 다른 곳 클릭하면 레이아웃 원래대로 복구.. 어케 함?
 
         // 위치 초기화 (기준은 cafeId = 1)
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cafe1, 14f))
-        setCurrentLocation()
-
     }
 
-    fun setCurrentLocation(){
-        val lm = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        if (ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
-        }
-        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 10.0f, locationListener)
-    }
-    val locationListener = object : android.location.LocationListener {
-        override fun onLocationChanged(location: Location) {
-            // 위치가 변경되었을 때 호출됩니다.
-            val latitude = location.latitude
-            val longitude = location.longitude
-        }
+    fun updateNearCafe(tag: Int) {
+        binding.tvHomeLabel.visibility = View.GONE
+        binding.vHomeLine.visibility = View.GONE
+        binding.rvHome.visibility = View.GONE
 
-        override fun onLocationChanged(locations: MutableList<Location>) {
-            super.onLocationChanged(locations)
-            //위치가 변경되어 위치가 일괄 전달될 때 호출됩니다.
-        }
-        override fun onProviderDisabled(provider: String) {
-            super.onProviderDisabled(provider)
-            // 사용자가 GPS를 끄는 등의 행동을 해서 위치값에 접근할 수 없을 때 호출됩니다.
-        }
-
-        override fun onProviderEnabled(provider: String) {
-            super.onProviderEnabled(provider)
-            // 사용자가 GPS를 on하는 등의 행동을 해서 위치값에 접근할 수 있게 되었을 때 호출됩니다.
-        }
-
-        override fun onFlushComplete(requestCode: Int) {
-            super.onFlushComplete(requestCode)
-            //플러시 작업이 완료되고 플러시된 위치가 전달된 후 호출됩니다.
-        }
+        binding.clHomeNearCafe.visibility = View.VISIBLE
+//        id가 tag인 cafe 불러와서 text랑 기타 등등.. 세팅
+//        binding.nearCafeName.text =
     }
 }
