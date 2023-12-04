e88335f6df70a01bc32873670ff36d41e628d2b8
dpwlsz <dpwls0875@konkuk.ac.kr>
2023-12-04 22:29:31 +0900

    feat(play)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,6 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
